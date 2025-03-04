d4182d3856fa53b08d064841d0673d3b7a19594e
wanfeng <804121985@qq.com>
2025-03-04 11:34:01 +0800

    ui

--- notebook/widget.cpp
+++ notebook/widget.cpp
@@ -5,7 +5,10 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
+    // ui绑定widget
     ui->setupUi(this);
+    // 让widget窗口变化时内部布局也随着变化
+    this->setLayout(ui->verticalLayout);
 }
 
 Widget::~Widget()
