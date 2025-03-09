a11215bc806c5de8e54e4dd7aaef484add22bcc9
wanfeng <804121985@qq.com>
2025-03-10 01:49:45 +0800

    learn: render

--- OpenGL/README.md
+++ OpenGL/README.md
@@ -5,5 +5,23 @@
 - NDC:标准化设备坐标，按比例缩放
 - VBO(Vertex Buffer Object):表示了GPU上的一段存储空间对象，在Cpp中表现为unsigned int变量，表示成为GPU端内存对象的一个ID编号
 - VAO(Vertex Array Object):顶点数组对象，用于存储一个Mesh网格所有的顶点属性描述信息
-## GLFW
-## GLAD
+# 库
+- GLFW
+- GLAD
+
+## Shader
+`对三角形数据的处理，分为顶点处理和片元处理`
+- VertexShader
+  layout(location = 0) in vec3 aPos; 
+    - in：输入 
+    - vec3:三维向量 
+    - aPos:变量名  
+    - 表示输入的三维变量是aPos 
+    - layout(location = 0)：表示VertexShader去VAO的第n个属性描述中去取数据
+  gl_Position = vec4(aPosx,aPosy,aPosz,1.0)
+    - gl_Position：glsl内置变量
+    - 负责后续阶段输出顶点位置处理的结果
+- FragmentShader
+    当顶点处理完成后，会定位需要处理的片元，每个片元进入一个core，执行FragmentShader
+- GLSL语言（Graphic Library Shader Language 类c语言，为图形学计算量身定制） 将输入转化为输出的程序
+  - 需要在代码中写源码，编译，链接
--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -1,6 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
+GLuint vao,program;
+
+
 // 定义 GL_CALL 宏
 #define GL_CALL(x) \
     do { \
@@ -24,7 +28,8 @@ void learnVBO(){
     float vertices[]={
             -0.5f, -0.5f, 0.0f,1.0f,1.0f,1.0f,
             0.5f, -0.5f, 0.0f,1.0f,1.0f,1.0f,
-            0.0f, 0.5f, 0.0f,1.0f,1.0f,1.0f
+            0.0f, 0.5f, 0.0f,1.0f,1.0f,1.0f,
+            0.3f, 0.3f, 0.3f,1.0f,1.0f,1.0f
     };
     // 创建VBO,glGenBuffers后会设置一个id
     GLuint VBO = 0;
@@ -38,7 +43,7 @@ void learnVBO(){
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
 
     // 构建VAO
-    GLuint vao = 0;
+    vao = 0;
     // 生成id
     glGenVertexArrays(1,&vao);
     // 绑定opengl当前状态的vao
@@ -57,6 +62,84 @@ void learnVBO(){
     glBindBuffer(GL_ARRAY_BUFFER,0);
 }
 
+//shader
+void shaderLearn(){
+    // glsl源码
+    const char* vertexShaderSource = "#version 460 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "layout (location = 1) in vec3 aColor;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z,1.0);\n"
+                                     "}\n";
+    const char* frameShaderSource = "#version 330 core\n"
+                                     "out vec4 FragColor;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   FragColor = vec4(0.1f,0.8f,0.2f,1.0f);\n"
+                                     "}\n";
+    // 创建shader程序
+    GLuint vertex,fragment;
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // 源码输入
+    glShaderSource(vertex,1,&vertexShaderSource,NULL);
+    glShaderSource(fragment,1,&frameShaderSource,NULL);
+
+    int success = 0;
+    char infoLog[1024];
+    // 编译源码
+    glCompileShader(vertex);
+    // 检查
+    glGetShaderiv(vertex,GL_COMPILE_STATUS,&success);
+    if (!success){
+        glGetShaderInfoLog(vertex,1024,NULL,infoLog);
+        std::cout << "fail to compile vertex shader" <<infoLog<< std::endl;
+    }
+    // 编译源码
+    glCompileShader(fragment);
+    // 检查
+    glGetShaderiv(fragment,GL_COMPILE_STATUS,&success);
+    if (!success){
+        glGetShaderInfoLog(fragment,1024,NULL,infoLog);
+        std::cout << "fail to compile fragment shader" <<infoLog<< std::endl;
+    }
+
+    // 链接
+    // 创建程序壳子
+    program = glCreateProgram();
+    // 编译结果放入pro
+    glAttachShader(program,vertex);
+    glAttachShader(program,fragment);
+    // 链接操作
+    glLinkProgram(program);
+    // 检查链接错误
+    glGetProgramiv(program,GL_LINK_STATUS,&success);
+    if (!success){
+        glGetProgramInfoLog(program,1024,NULL,infoLog);
+        std::cout << "fail to link program" <<infoLog<< std::endl;
+    }
+
+    // 扫尾工作
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+// 渲染
+void render(){
+    // 画布清理
+    GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
+    // 绑定program
+    glUseProgram(program);
+    // 绑定vao
+    glBindVertexArray(vao);
+    // 绘制
+    glDrawArrays(GL_TRIANGLES,0,3);
+    // 解绑
+    glBindVertexArray(0);
+    glUseProgram(0);
+}
 
 int main()
 {
@@ -78,7 +161,8 @@ int main()
         std::cout << "fail to init glad";
         return -1;
     }
-
+    // shader程序
+    shaderLearn();
     // viewport 视口，表示渲染的范围
     glViewport(0,0,800,600);
     // 设置清理颜色
@@ -90,10 +174,8 @@ int main()
 
         // 接收并分发窗口消息
         glfwPollEvents();
-
-        // 画布清理
-        GL_CALL(glClear(GL_COLOR_BUFFER_BIT));
-
+        // 渲染
+        render();
         // 更换双缓存
         glfwSwapBuffers(window);
     }
