5161ca9491d6a1066efa16edc642d4c2b2214e21
liuzhuohao <804121985@qq.com>
2025-04-03 17:32:58 +0800

    fix shadow mapping

--- tinyrenderer/main.cpp
+++ tinyrenderer/main.cpp
@@ -129,7 +129,6 @@ void shadow(Triangle &triangle,std::vector<std::vector<float>> *shadowBuffer){
     int bbmaxx = std::floor(std::max(std::max(ax, bx), cx));
     int bbmaxy = std::ceil(std::max(std::max(ay, by), cy));
     float total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
-
     for (int x = bbminx; x <= bbmaxx; x++) {
         for (int y = bbminy; y <= bbmaxy; y++) {
             // 虽然可以把整个三角形直接剔除，但是我希望只是把屏幕外的像素剔除
@@ -153,77 +152,77 @@ void shadow(Triangle &triangle,std::vector<std::vector<float>> *shadowBuffer){
 
 
 // 绘制一个三角形
-    void drawTriangle(Triangle &triangle, TGAImage &framebuffer, std::vector<std::vector<float>> *zBuffer, std::vector<std::vector<float>> *shadowBuffer,Texture &texture,
-                 Texture &nm, Texture &spec, Texture &nm_tangent,Eigen::Matrix4f mvpForShadow) {
-        float ax = triangle.screenCoords[0].x();
-        float ay = triangle.screenCoords[0].y();
-        float bx = triangle.screenCoords[1].x();
-        float by = triangle.screenCoords[1].y();
-        float cx = triangle.screenCoords[2].x();
-        float cy = triangle.screenCoords[2].y();
-        int bbminx = std::floor(std::min(std::min(ax, bx), cx));
-        int bbminy = std::ceil(std::min(std::min(ay, by), cy));
-        int bbmaxx = std::floor(std::max(std::max(ax, bx), cx));
-        int bbmaxy = std::ceil(std::max(std::max(ay, by), cy));
+void drawTriangle(Triangle &triangle, TGAImage &framebuffer, std::vector<std::vector<float>> *zBuffer, std::vector<std::vector<float>> *shadowBuffer,Texture &texture,
+             Texture &nm, Texture &spec, Texture &nm_tangent,Eigen::Matrix4f mvpForShadow) {
+    float ax = triangle.screenCoords[0].x();
+    float ay = triangle.screenCoords[0].y();
+    float bx = triangle.screenCoords[1].x();
+    float by = triangle.screenCoords[1].y();
+    float cx = triangle.screenCoords[2].x();
+    float cy = triangle.screenCoords[2].y();
+    int bbminx = std::floor(std::min(std::min(ax, bx), cx));
+    int bbminy = std::ceil(std::min(std::min(ay, by), cy));
+    int bbmaxx = std::floor(std::max(std::max(ax, bx), cx));
+    int bbmaxy = std::ceil(std::max(std::max(ay, by), cy));
 
-        // 如果面积为负数，背对屏幕，被裁剪
-        float total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
-        if (total_area < 1) return;
+    // 如果面积为负数，背对屏幕，被裁剪
+    float total_area = signed_triangle_area(ax, ay, bx, by, cx, cy);
+    if (total_area < 1) return;
 
 #pragma omp parallel for
-        for (int x = bbminx; x <= bbmaxx; x++) {
-            for (int y = bbminy; y <= bbmaxy; y++) {
-                // 虽然可以把整个三角形直接剔除，但是我希望只是把屏幕外的像素剔除
-                if (x < 0 || x >= width || y < 0 || y >= height) {
-                    continue;
-                }
-                float alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
-                float beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
-                float gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
-                if (alpha < 0 || beta < 0 || gamma < 0) continue; // 说明当前像素不在三角形内部
-                float barycentricZ = interpolate(triangle.screenCoords[0].z(), triangle.screenCoords[1].z(),triangle.screenCoords[2].z(), alpha, beta, gamma);
-                Eigen::Vector3f barycentricGlobalCoord = interpolate(triangle.globalCoords[0].head<3>(),triangle.globalCoords[1].head<3>(),triangle.globalCoords[2].head<3>(), alpha, beta,gamma);
-                float texU = interpolate(triangle.texCoords[0].x(), triangle.texCoords[1].x(),triangle.texCoords[2].x(), alpha, beta, gamma);
-                float texV = interpolate(triangle.texCoords[0].y(), triangle.texCoords[1].y(),triangle.texCoords[2].y(), alpha, beta, gamma);
-                TGAColor texColor = texture.getColor(texU, texV);
-                Eigen::Vector3f barycentricNorm = interpolate(triangle.normal[0], triangle.normal[1],triangle.normal[2], alpha, beta, gamma);
-                // 法线来自法线贴图
+    for (int x = bbminx; x <= bbmaxx; x++) {
+        for (int y = bbminy; y <= bbmaxy; y++) {
+            // 虽然可以把整个三角形直接剔除，但是我希望只是把屏幕外的像素剔除
+            if (x < 0 || x >= width || y < 0 || y >= height) {
+                continue;
+            }
+            float alpha = signed_triangle_area(x, y, bx, by, cx, cy) / total_area;
+            float beta = signed_triangle_area(x, y, cx, cy, ax, ay) / total_area;
+            float gamma = signed_triangle_area(x, y, ax, ay, bx, by) / total_area;
+            if (alpha < 0 || beta < 0 || gamma < 0) continue; // 说明当前像素不在三角形内部
+            float barycentricZ = interpolate(triangle.screenCoords[0].z(), triangle.screenCoords[1].z(),triangle.screenCoords[2].z(), alpha, beta, gamma);
+            Eigen::Vector3f barycentricGlobalCoord = interpolate(triangle.globalCoords[0].head<3>(),triangle.globalCoords[1].head<3>(),triangle.globalCoords[2].head<3>(), alpha, beta,gamma);
+            float texU = interpolate(triangle.texCoords[0].x(), triangle.texCoords[1].x(),triangle.texCoords[2].x(), alpha, beta, gamma);
+            float texV = interpolate(triangle.texCoords[0].y(), triangle.texCoords[1].y(),triangle.texCoords[2].y(), alpha, beta, gamma);
+            TGAColor texColor = texture.getColor(texU, texV);
+            Eigen::Vector3f barycentricNorm = interpolate(triangle.normal[0], triangle.normal[1],triangle.normal[2], alpha, beta, gamma);
+            // 法线来自法线贴图
 //            Eigen::Vector3f barycentricNorm = TGAColorToVector3f(nm.getColor(texU,texV))*2-Vector3f{1,1,1};
-                // 切线法线贴图
+            // 切线法线贴图
 //                Eigen::Vector3f barycentricNmTangent =TGAColorToVector3f(nm_tangent.getColor(texU, texV)) * 2 - Vector3f{1, 1, 1};
 //                barycentricNmTangent = getNormalFromTangent(triangle, barycentricNmTangent, barycentricNorm);
-                // 高光系数来自高光贴图
-                TGAColor specKd = spec.getColor(texU, texV);
-                // zbuffer中缓存的渲染物体距离小于当前渲染物体的距离时，才覆盖渲染
-                if (zBuffer->at(x).at(y) < barycentricZ) {
-                    zBuffer->at(x).at(y) = barycentricZ;
-                    // 阴影处理
-                    // 1. 找到该像素对应物体在原空间的位置
-                    Eigen::Vector4f locationInShaowBuffer = mvpForShadow * (barycentricGlobalCoord.homogeneous());
-                    locationInShaowBuffer.x() /= locationInShaowBuffer.w();
-                    locationInShaowBuffer.y() /= locationInShaowBuffer.w();
-                    locationInShaowBuffer.z() /= locationInShaowBuffer.w();
-                    locationInShaowBuffer.x() = 0.5*width*(locationInShaowBuffer.x()+1);
-                    locationInShaowBuffer.y() = 0.5*height*(locationInShaowBuffer.y()+1);
-                    bool isShadow = false;
-                    // 在阴影中
-                    if (locationInShaowBuffer.z() < shadowBuffer->at(locationInShaowBuffer.x()).at(locationInShaowBuffer.y())){
-                        isShadow = true;
-                    }
-                    // 直接使用贴图
-//                framebuffer.set(x,y, texture.getColor(texU,texV));
-                    // 使用phongshading光照模型
-                    framebuffer.set(x,y, blinnPhongShading(texColor,barycentricGlobalCoord,barycentricNorm,specKd,isShadow));
-                    // 直接使用法线贴图
-//                framebuffer.set(x,y, nm.getColor(texU,texV));
-                    // 使用法线贴图的法线配合phongshading
-//                framebuffer.set(x,y, blinnPhongShading(texColor,barycentricGlobalCoord,barycentricNorm,specKd));
-                    // 使用切线法线贴图配合phongshaing
-//                    framebuffer.set(x, y,blinnPhongShading(texColor, barycentricGlobalCoord, barycentricNmTangent, specKd));
+            // 高光系数来自高光贴图
+            TGAColor specKd = spec.getColor(texU, texV);
+
+            // zbuffer中缓存的渲染物体距离小于当前渲染物体的距离时，才覆盖渲染
+            if (zBuffer->at(x).at(y) < barycentricZ) {
+                zBuffer->at(x).at(y) = barycentricZ;
+                // 阴影处理
+                Eigen::Vector4f locationInShadowBuffer = mvpForShadow * (barycentricGlobalCoord.homogeneous());
+                locationInShadowBuffer.x() /= locationInShadowBuffer.w();
+                locationInShadowBuffer.y() /= locationInShadowBuffer.w();
+                locationInShadowBuffer.z() /= locationInShadowBuffer.w();
+                locationInShadowBuffer.x() = 0.5*width*(locationInShadowBuffer.x()+1);
+                locationInShadowBuffer.y() = 0.5*height*(locationInShadowBuffer.y()+1);
+                bool isShadow = false;
+                // 在阴影中
+                if (locationInShadowBuffer.z()+0.005 < shadowBuffer->at(locationInShadowBuffer.x()).at(locationInShadowBuffer.y())){
+                    isShadow = true;
                 }
+                // 直接使用贴图
+//                framebuffer.set(x,y, texture.getColor(texU,texV));
+                // 使用phongshading光照模型
+                framebuffer.set(x,y, blinnPhongShading(texColor,barycentricGlobalCoord,barycentricNorm,specKd,isShadow));
+                // 直接使用法线贴图
+//                framebuffer.set(x,y, nm.getColor(texU,texV));
+                // 使用法线贴图的法线配合phongshading
+//                framebuffer.set(x,y, blinnPhongShading(texColor,barycentricGlobalCoord,barycentricNorm,specKd));
+                // 使用切线法线贴图配合phongshaing
+//                    framebuffer.set(x, y,blinnPhongShading(texColor, barycentricGlobalCoord, barycentricNmTangent, specKd));
             }
         }
     }
+}
 
     int main() {
         Model model("./obj/diablo3_pose/diablo3_pose.obj", "./obj/diablo3_pose/diablo3_pose_diffuse.tga");
