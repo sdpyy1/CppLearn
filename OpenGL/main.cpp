3547e901c37edcaf33bf3b2cf6cf8fa74d6bd554
wanfeng <804121985@qq.com>
2025-04-04 22:23:29 +0800

    shader done!

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -4,6 +4,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <valarray>
+#include "Shader.h"
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     std::cout << "Failed to create GLFW window" << std::endl;
     glViewport(0, 0, width, height);
@@ -33,8 +36,49 @@ GLFWwindow * InitWindowAndFunc(){
 
 int main()
 {
+    float vertices[] = {
+            0.5f, 0.5f, 0.0f,   // 右上角
+            0.5f, -0.5f, 0.0f,  // 右下角
+            -0.5f, -0.5f, 0.0f, // 左下角
+            -0.5f, 0.5f, 0.0f   // 左上角
+    };
+
+    unsigned int indices[] = {
+            // 注意索引从0开始!
+            // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+            // 这样可以由下标代表顶点组合成矩形
+
+            0, 1, 3, // 第一个三角形
+            1, 2, 3  // 第二个三角形
+    };
 
     GLFWwindow * window = InitWindowAndFunc();
+
+    // 创建一个ID
+    GLuint VBO;
+    // 此函数会为VBO分配一个未使用的ID（例如 1, 2 等），但此时‌并未实际创建缓冲对象‌，仅预留了标识符
+    glGenBuffers(1, &VBO);
+    // 绑定到OpenGL上下文中，此时GPU才会真正分配内存
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBufferData是一个专门用来把用户定义的数据复制到当前绑定缓冲的函数。它的第一个参数是目标缓冲的类型：顶点缓冲对象当前绑定到GL_ARRAY_BUFFER目标上。第二个参数指定传输数据的大小(以字节为单位)；用一个简单的sizeof计算出顶点数据大小就行。第三个参数是我们希望发送的实际数据。
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+    // VAO
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // EBO
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    Shader ourShader("./shader/shader.vs", "./shader/shader.fs");
+
+
     while(!glfwWindowShouldClose(window))
     {
         // 双缓冲
@@ -43,6 +87,14 @@ int main()
         glfwPollEvents();
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        // 更新uniform颜色
+        ourShader.use();
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        ourShader.setVec4f("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
+
+        // 通过绑定好的VAO和VBO和EBO画三角形
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
     glfwTerminate();
     return 0;
