cbd57c20c6e5b62c750c21ee1684477fcea6e496
wanfeng <804121985@qq.com>
2025-04-13 20:57:13 +0800

    3d texture model

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -14,18 +14,21 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 GLFWwindow * InitWindowAndFunc();
 GLuint loadTexture(char const * path);
-void drawModel(Shader &bagShader, Model &model, glm::vec3 translate, glm::vec3 scale) ;// view/projection transformations
+void drawModel(Shader &shader, GLuint &VAO, glm::vec3 translate, glm::vec3 scale) ;// view/projection transformations
 
 void processFrameTimeForMove();
+
+GLuint getCubeVAO();
+
+GLuint getSkyVAO();
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 5.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 8.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 // timing
 float deltaTime = 0.0f;	// time between current frame and last frame
 float lastFrame = 0.0f;
@@ -36,42 +39,63 @@ int main(){
     stbi_set_flip_vertically_on_load(true);
     // 启用深度测试
     glEnable(GL_DEPTH_TEST);
-    // 启动面剔除
-    glEnable(GL_CULL_FACE);
-
 
     // 着色器设置
-    Shader bagShader("./shader/bag.vert", "./shader/bag.frag");
-    // 模型导入
-    Model model = Model("./assets/backpack/backpack.obj");
+    Shader cubeShader("./shader/bag.vert", "./shader/bag.frag");
+    Shader skyShader("./shader/sky.vert", "./shader/sky.frag");
 
+    GLuint CubeVAO = getCubeVAO();
+    GLuint skyVAO = getSkyVAO();
 
-
+    GLuint mutouTexture = loadTexture("./assets/diffuse.png");
+    GLuint awesomeTexture = loadTexture("./assets/awesomeface.png");
     vector<std::string> faces
             {
-                    "right.jpg",
-                    "left.jpg",
-                    "top.jpg",
-                    "bottom.jpg",
-                    "front.jpg",
-                    "back.jpg"
+                    "./assets/skybox/right.jpg",
+                    "./assets/skybox/left.jpg",
+                    "./assets/skybox/top.jpg",
+                    "./assets/skybox/bottom.jpg",
+                    "./assets/skybox/front.jpg",
+                    "./assets/skybox/back.jpg"
             };
-    GLuint cubemapTexture = loadCubemap(faces);
-
-
-
+    GLuint skyTexture = loadCubemap(faces);
 
     while (!glfwWindowShouldClose(window))
     {
 
         // 清理窗口
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-
-        // 这里开始都是在FBO上渲染
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        // 绘制物体
-        drawModel(bagShader, model,{1.f,0.f,-3.f},{1.0f,1.0f,1.0f});
 
+        // 绘制不透明物体
+        // 给物体上反射贴图
+        cubeShader.use();
+        glBindVertexArray(CubeVAO);
+        cubeShader.setVec3("cameraPos", camera.Position);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mutouTexture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, awesomeTexture);
+        glActiveTexture(GL_TEXTURE2);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skyTexture);
+        cubeShader.setInt("texture1", 0);
+        cubeShader.setInt("texture2", 1);
+        cubeShader.setInt("skybox", 2);
+        drawModel(cubeShader, CubeVAO,{0,0,0},{1,1,1});
+
+        // 渲染透明物体
+
+        // 渲染天空
+        glDepthFunc(GL_LEQUAL);
+        skyShader.use();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
+        skyShader.setMat4("projection", projection);
+        skyShader.setMat4("view", view);
+        glBindVertexArray(skyVAO);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skyTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDepthFunc(GL_LESS);
 
         // 事件处理
         glfwPollEvents();
@@ -86,25 +110,145 @@ int main(){
     return 0;
 }
 
+GLuint getSkyVAO() {
+    float skyboxVertices[] = {
+            // positions
+            -1.0f,  1.0f, -1.0f,
+            -1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f, -1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+
+            -1.0f, -1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f,
+            -1.0f, -1.0f,  1.0f,
+
+            -1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f, -1.0f,
+            1.0f,  1.0f,  1.0f,
+            1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f,  1.0f,
+            -1.0f,  1.0f, -1.0f,
+
+            -1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f, -1.0f,
+            1.0f, -1.0f, -1.0f,
+            -1.0f, -1.0f,  1.0f,
+            1.0f, -1.0f,  1.0f
+    };
+    // sky的VAO
+    GLuint skyVAO, skyVBO;
+    glGenVertexArrays(1, &skyVAO);
+    glBindVertexArray(skyVAO);
+    glGenBuffers(1, &skyVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+    return skyVAO;
+}
+
+GLuint getCubeVAO() {
+    float cubeVs[] = {
+            // positions          // normals           // texture coords
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+            0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+            0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+            0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
+            0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+            0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+            0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+            0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+            0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+            0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
+    };
+
+    GLuint CubeVBO, CubeVAO;
+    glGenBuffers(1, &CubeVBO);
+    glGenVertexArrays(1, &CubeVAO);
+    glBindVertexArray(CubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, CubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVs), &cubeVs, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    glBindVertexArray(0);
+    return CubeVAO;
+}
+
+void drawModel(Shader &shader, GLuint &VAO, glm::vec3 translate, glm::vec3 scale) {
+
+    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    glm::mat4 view = camera.GetViewMatrix();
+    shader.setMat4("projection", projection);
+    shader.setMat4("view", view);
+    glm::mat4 modelTrans = glm::mat4(1.0f);
+    modelTrans = glm::translate(modelTrans, translate);
+    modelTrans = glm::scale(modelTrans, scale);
+    shader.setMat4("model", modelTrans);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+};
+
 void processFrameTimeForMove() {
     auto currentFrame = static_cast<float>(glfwGetTime());
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
 }
 
-void drawModel(Shader &bagShader, Model &model, glm::vec3 translate, glm::vec3 scale) {
-    bagShader.use();
-    glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-    glm::mat4 view = camera.GetViewMatrix();
-    bagShader.setMat4("projection", projection);
-    bagShader.setMat4("view", view);
-    // render the loaded model
-    glm::mat4 modelTrans = glm::mat4(1.0f);
-    modelTrans = glm::translate(modelTrans, translate); // translate it down so it's at the center of the scene
-    modelTrans = glm::scale(modelTrans, scale);    // it's a bit too big for our scene, so scale it down
-    bagShader.setMat4("model", modelTrans);
-    model.Draw(bagShader);
-};
 
 GLFWwindow * InitWindowAndFunc() {
     glfwInit();
@@ -240,6 +384,8 @@ unsigned int loadCubemap(vector<std::string> faces)
     int width, height, nrChannels;
     for (unsigned int i = 0; i < faces.size(); i++)
     {
+        //反转
+        stbi_set_flip_vertically_on_load(false);
         unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
         if (data)
         {
--- OpenGL/shader/bag.frag
+++ OpenGL/shader/bag.frag
@@ -1,13 +1,22 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
-
-uniform sampler2D texture_diffuse1;
-
-const float offset = 1.0 / 300.0;
-
+in vec3 Normal;
+in vec3 Position;
+in vec2 texCoords;
+uniform vec3 cameraPos;
+uniform samplerCube skybox;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 void main()
 {
-    FragColor = texture(texture_diffuse1, TexCoords);
+    vec3 I = normalize(Position - cameraPos);
+    FragColor = vec4(mix(texture(texture1,texCoords), texture(texture2,texCoords),0.5));
+    if(dot(-I, Normal)>0){
+        float ratio = 1.00 / 1.52;
+
+        vec3 R = refract(I, normalize(Normal), ratio);
+        FragColor = mix(texture(skybox, R), FragColor, 0.5);
+    }
+
 }
